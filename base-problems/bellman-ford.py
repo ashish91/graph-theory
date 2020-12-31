6eb8805da36b7ebedf21130ef00216627c893e5c
Ashish Gaur <ashishgaurp91@gmail.com>
2021-01-01 00:47:48 +0530

    Remaining SSSP

---
+++ base-problems/bellman-ford.py
@@ -0,0 +1,23 @@
+# Time Complexity: O(EV)
+def bellman_ford(S, edgelist, N):
+	dist = [float('infinity')] * N
+	dist[S] = 0
+
+	# For each vertice
+	for v in range(N):
+		# Relax all edges
+		for edge in edgelist:
+			if dist[edge.fro] + edge.weight < edge.to:
+				dist[edge.to] = dist[edge.fro] + edge.weight
+
+	# Find neg cycles. If a node is part
+	# of neg cycle then dist will be less than
+	# the optimal solution
+	for v in range(N):
+		for edge in edgelist:
+			if dist[edge.fro] + edge.weight < edge.to:
+				dist[edge.to] = float('-infinity')
+
+	return dist
+
+
